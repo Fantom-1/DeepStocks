f3dd41295a34485f8ec56e87ab616d42489d4651
Ashwin-Rathore-07 <rathoreashwin179@gmail.com>
2025-04-22 16:39:03 +0530

    profile added

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,11 +1,19 @@
 // Profile Popup Functionality
 document.addEventListener('DOMContentLoaded', function() {
+    const UserProfile= document.getElementById('userProfile')
     const profileLink = document.getElementById('sidebarProfileLink');
     const profilePopup = document.getElementById('profilePopup');
     const closeProfilePopup = document.getElementById('closeProfilePopup');
     
-    if (profileLink && profilePopup) {
+    if (profileLink && profilePopup || UserProfile) {
         // Toggle profile popup when clicking profile link in sidebar
+        UserProfile.addEventListener('click', function(e) {
+            e.preventDefault();
+            profilePopup.style.display = 'flex';
+            document.body.style.overflow = 'hidden';
+        });
+
+
         profileLink.addEventListener('click', function(e) {
             e.preventDefault();
             profilePopup.style.display = 'flex';
